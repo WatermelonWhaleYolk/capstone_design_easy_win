95e65b87ea335c7738605e9de5eda5961ba6b95a
WatermelonWhaleYolk <kjkj5757@naver.com>
2025-03-27 14:20:19 +0900

    20250327

--- frontend/src/component/home/header/header.js
+++ frontend/src/component/home/header/header.js
@@ -2,7 +2,14 @@ import './header.css'
 
 export default function Header() {
   return (
-    <div className="header">
-    </div>
+      <nav className="header">
+        <img src="../../../asset/kiumascot.png" className="header_logo" />
+        <nav className="nav_bar">Go KIU</nav>
+        <nav className="nav_bar">봉사기록 확인</nav>
+        <nav className="nav_bar">ABOUT_US</nav>
+        <nav className="nav_bar">
+          <button className="login_btn">로그인</button>
+          </nav>
+      </nav>
   )
 };
